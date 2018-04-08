bce092d8ecc2c990a932924eb991a79764b6344a
John Lin <jlin22@stuy.edu>
2018-04-08 13:10:20 -0400

    done i think

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ screen = new_screen()
 color = [ 0, 255, 0 ]
 edges = []
 transform = new_matrix()
+
+parse_file( 'script', edges, transform, screen, color )	
 '''
 add_box(edges, 200, 200, 200, 100, 100, 100)
 add_sphere(edges, 250, 250, 250, 50, 10)
@@ -15,4 +17,3 @@ add_torus(edges, 250, 250, 250, 50, 75, 10)
 draw_polygons(edges, screen, color)
 save_ppm(screen, 'img.ppm')
 '''
-parse_file( 'script', edges, transform, screen, color )	
